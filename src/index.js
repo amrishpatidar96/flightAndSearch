bc4f4daaf4d00531ad20a6c1b9d6a115de7160f2
ampatidar <ampatidar@gammastack.com>
2026-04-17 17:21:56 +0530

    body parser  added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const {PORT} = require('./configs/serverConfig');
+const bodyParser = require('body-parser');
 const setupAndStartServer = async ()=>{
 
     const app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
     app.listen(PORT,()=>{
         console.log(`Server is running on port ${PORT}`);
     })
